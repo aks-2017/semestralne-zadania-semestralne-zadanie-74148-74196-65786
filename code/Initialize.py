719dc8f7050b922b4b69f16f9e7ee9790684551d
Marko Ondrus <marko.ondrus@gmail.com>
2017-11-20 02:12:21 +0100

    Functioning Simple GET req.

--- code/Initialize.py
+++ code/Initialize.py
@@ -3,16 +3,18 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_0
+import requests
 
 class L2Switch(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(L2Switch, self).__init__(*args, **kwargs)
 
         print ("klobasa")
-        printf ("nova brancha 2")
+        print ("nova brancha 2")
 
         @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-        def packet_in_handler(self, ev):
+        def _packet_in_handler(self, ev):
+            msg = ev.msg
             #msg = ev.msg
             #dp = msg.datapath
             #ofp = dp.ofproto
@@ -23,4 +25,16 @@ class L2Switch(app_manager.RyuApp):
             #    datapath=dp, buffer_id=msg.buffer_id, in_port=msg.in_port,
             #    actions=actions)
             #dp.send_msg(out)
+
+            print (msg)
+
+            r = requests.get('http://localhost:8080/stats/switches')
+            r.status_code
+
+            # extracting data in json format
+            data = r.json()
+
+            print (data)
+
+
             print ("salama")
